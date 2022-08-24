f6866ddad61ea4807b39431364eade262bb669a2
juyal.priyanka94@gmail.com <juyal.priyanka94@gmail.com>
2022-08-24 17:16:30 +0530

    Updated the username

--- Person.py
+++ Person.py
@@ -12,15 +12,15 @@ class Person:
 
     def get_name(self, user_id):
         if user_id >= len(self.name):
-            return 'There is no such user'
+            return 'There is no such user.'
         else:
             return self.name[user_id]
 
 
 if __name__ == '__main__':
     person = Person()
-    print('User Abbas has been added with id ', person.set_name('Abbas'))
-    print('User associated with id 0 is ', person.get_name(0))
+    print('User has been added with id ', person.set_name('Priyanka'))
+    print('User associated with id is ', person.get_name(0))
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
